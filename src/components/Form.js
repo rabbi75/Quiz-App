3f98f4f2504c7e2b538b4e4063c35e01dafd94be
Golam Rabbi <spirabbi@gmail.com>
2022-01-11 12:28:20 +0600

    Upload full project

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "../styles/From.module.css";
+
+export default function Form({ children, className, ...rest }) {
+  return (
+    <form className={`${className} ${classes.form}`} action="#" {...rest}>
+      {children}
+    </form>
+  );
+}
---
+++ src/components/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Illustration from "../Illustration";
+import SignupForm from "../SignupForm";
+
+export default function Signup() {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div class="column">
+        <Illustration />
+        <SignupForm />
+      </div>
+    </>
+  );
+}
